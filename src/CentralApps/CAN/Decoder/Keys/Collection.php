e307554181f651c7062f4744002d278ae869dc44
Michael Peacock <michael@centralapps.co.uk>
2012-01-22 01:36:12 +0000

    Making the decoder keys collection implement the iteratoraggregate and countable interfaces

--- src/CentralApps/CAN/Decoder/Keys/Collection.php
+++ src/CentralApps/CAN/Decoder/Keys/Collection.php
@@ -1,9 +1,23 @@
 <?php
 namespace CentralApps\CAN;
-class Decoder_Keys_Collection {
+class Decoder_Keys_Collection implements \IteratorAggregate, \Countable {
 	
 	private $collection;
 	
+	public function getIterator()
+	{
+		return new \ArrayIterator( $this->collection );
+	}
+	
+	public function add( $object )
+	{
+		$this->collection[] = $object;
+	}
+	
+	public function count()
+	{
+		return count( $this->collection );
+	}
 	
 	
 }
