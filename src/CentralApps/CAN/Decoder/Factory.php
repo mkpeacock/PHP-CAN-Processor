42fbc12af23514d694ac9812202dfa119059a502
Michael Peacock <michael@centralapps.co.uk>
2012-01-22 17:47:26 +0000

    Initial work on decoder factory

---
+++ src/CentralApps/CAN/Decoder/Factory.php
@@ -0,0 +1,30 @@
+<?php
+namespace CentralApps\CAN;
+// class to build the decoder collection
+class Decoder_Factory {
+	
+	// take a particular CAN Message and lookup the decoding keys for variables held within
+	// push these keys into a collection and return
+	public function makeDecoderFromCan( Message $message )
+	{
+		$canID = $message->getCanID();
+		return new Decoder_Collection();
+	}
+	
+	public function makeAllDecoders()
+	{
+		return new Decoder_Collection();
+	}
+	
+	private function makeFrom( $sqlStatement )
+	{
+		try {
+			// querying for the information
+		}
+		catch ( Exception $e ){
+			//
+		}
+		return new Decoder_Collection();
+	}
+	
+}
