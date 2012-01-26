df181593a9608e4e1f40fd09ec4bf8aff7ef1e43
Michael Peacock <michael@centralapps.co.uk>
2012-01-26 23:55:53 +0000

    multi byte works

--- src/CentralApps/CAN/Decoder/Keys/MultiByte.php
+++ src/CentralApps/CAN/Decoder/Keys/MultiByte.php
@@ -3,8 +3,36 @@ namespace CentralApps\CAN;
 // represents a variable which spans two bytes in a "CAN string"
 class Decoder_Keys_MultiByte extends Decoder_Keys_Core {
 	
-	private $byteHigh;
-	private $byteLow;
+	private $mostSignificantByte;
+	private $leastSignificantByte;
+	
+	public function extractDataFromCAN( Message $message )
+	{
+		$mostSignificantBytePositionInMessage = $this->calculateBytesPositionInMessage( $this->mostSignificantByte );
+		$leastSignificantBytePositionInMessage = $this->calculateBytesPositionInMessage( $this->leastSignificantByte );
+		$messageData = $message->getMessage();
+		
+		$mostSignificantByte = substr( $messageData, $mostSignificantBytePositionInMessage, 2 );
+		$leastSignificantByte = substr( $messageData, $leastSignificantBytePositionInMessage, 2 );
+		
+		return $this->transformToEngineeringUnit( decbin( hexdec( $mostSignificantByte . $leastSignificantByte ) ) );
+	}
+	
+	public function calculateBytesPositionInMessage( $byte )
+	{
+		return $byte*2;
+	}
+	
+	
+	public function setMostSignificantByte( $byte )
+	{
+		$this->mostSignificantByte = $byte;
+	}
+	
+	public function setLeastSignificantByte( $byte )
+	{
+		$this->leastSignificantByte = $byte;
+	}
 	
 	
 }
