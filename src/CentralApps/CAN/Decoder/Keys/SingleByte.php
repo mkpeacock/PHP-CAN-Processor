b944031b73baf2a88e5436a24549970e8debe396
Michael Peacock <michael@centralapps.co.uk>
2012-01-26 23:56:12 +0000

    single byte works

--- src/CentralApps/CAN/Decoder/Keys/SingleByte.php
+++ src/CentralApps/CAN/Decoder/Keys/SingleByte.php
@@ -5,4 +5,22 @@ class Decoder_Keys_SingleByte extends Decoder_Keys_Core {
 	
 	private $byte;
 	
+	public function extractDataFromCAN( Message $message )
+	{
+		$bytesPositionInMessage = $this->calculateBytesPositionInMessage();
+		$messageData = $message->getMessage();
+		$byteData = substr( $messageData, $bytesPositionInMessage, 2 );
+		return $this->transformToEngineeringUnit( decbin( hexdec( $byteData ) ) );
+	}
+	
+	public function calculateBytesPositionInMessage()
+	{
+		return $this->byte*2;
+	}
+		
+	public function setByte( $byte )
+	{
+		$this->byte = $byte;
+	}
+	
 }
