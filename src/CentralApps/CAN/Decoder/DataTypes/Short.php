392ccc0cd1bf9329fafa6a33d7a01e27b08321da
Michael Peacock <michael@centralapps.co.uk>
2012-01-26 23:56:23 +0000

    fixed short calculations

--- src/CentralApps/CAN/Decoder/DataTypes/Short.php
+++ src/CentralApps/CAN/Decoder/DataTypes/Short.php
@@ -9,22 +9,22 @@ class Decoder_DataTypes_Short implements Decoder_DataTypes_Interface {
 	
 	private $numberOfBitsInData = 16;
 	
-	public function convertType( $data )
+	public function convertType( $dataAsBinary )
 	{
 		// TODO this decbin line is repeated, in Core.php (transformToEngineeringUnit), can we abstract
-		$binary = decbin($data);
+		$data = bindec($dataAsBinary);
 		// provided the value is 16 bits long, check to see if the most significant bit (msb) is bit 16
-		if( strlen( $binary) == $this->numberOfBitsInData && $binary{0} == "1" )
+		if( strlen( $dataAsBinary ) == $this->numberOfBitsInData && $dataAsBinary{0} == "1" )
 		{
 			// perform a two's compliment on the data to get the negative value
 			// -(pow(2, n) - N) where n is the number of bits and N is the number for which to find out its 2's complement
 			// see: http://www.binaryconvert.com/result_signed_short.html?
 			// see: http://php.net/manual/en/function.sprintf.php#92157
-			echo -(pow(2, $this->numberOfBitsInData ) - $data);
+			return -(pow(2, $this->numberOfBitsInData ) - $data);
 		}
 		else
 		{
-			echo $data;
+			return $data;
 		}
 	}
 	
