c82b2879d3e6596b023f5c584df19ea7af733ad4
Michael Peacock <michael@centralapps.co.uk>
2012-01-27 00:07:33 +0000

    Correctly calls the datatype convertor where appropriate

--- src/CentralApps/CAN/Decoder/Keys/Core.php
+++ src/CentralApps/CAN/Decoder/Keys/Core.php
@@ -1,6 +1,6 @@
 <?php
 namespace CentralApps\CAN;
-abstract class Decoder_Keys_Core extends Decoder_Keys_Core {
+abstract class Decoder_Keys_Core {
 	
 	protected $canID;
 	protected $name;
@@ -9,16 +9,51 @@ abstract class Decoder_Keys_Core extends Decoder_Keys_Core {
 	protected $offset;
 	protected $dataLength;
 	protected $dataType;
+	
+	public function setCanID( $canID )
+	{
+		$this->canID = $canID;
+	}
+	
+	public function setName( $name )
+	{
+		$this->name = $name;
+	}
+	
+	public function setUnit( $unit )
+	{
+		$this->unit = $unit;
+	}
+	
+	public function setMultiplier( $multiplier )
+	{
+		$this->multiplier = $multiplier;
+	}
+	
+	public function setOffset( $offset )
+	{
+		$this->offset = $offset;
+	}
+	
+	public function setDataLength( $dataLength )
+	{
+		$this->dataLength = $dataLength;
+	}
+	
+	public function setDataType( $type )
+	{
+		$this->dataType = $type;
+	}
 		
-	abstract public function extractDataFromCAN( CAN_Message $message ); 
+	abstract public function extractDataFromCAN( Message $message ); 
 	
 	// take a potentially nonesensical numeric value and convert it to meaningful engineering units
 	// so that the value reflects the data it represents
+	// incoming data is in binary format
 	protected function transformToEngineeringUnit( $data )
 	{
 		// check the ordering of these
 		$data = $this->applyTypeConversion( $data );
-		$data = bindec( $data );
 		$data = $this->applyMultiplier( $data );
 		$data = $this->applyOffset( $data );
 		return $data;
@@ -26,12 +61,19 @@ abstract class Decoder_Keys_Core extends Decoder_Keys_Core {
 	
 	protected function applyOffset( $data )
 	{
-		return $data - $thisoffset;
+		return $data- $this->offset;
 	}
 	
 	protected function applyMultiplier( $data )
 	{
-		return $data * $this->multiplier;
+		if( $this->multiplier <> 0 )
+		{
+			return $data * $this->multiplier;
+		}
+		else
+		{
+			return $data;
+		}		
 	}
 	
 	protected function applyTypeConversion( $data )
@@ -39,9 +81,13 @@ abstract class Decoder_Keys_Core extends Decoder_Keys_Core {
 		// TODO add logic here!
 		if( ! is_null( $this->dataType ) )
 		{
-			$convertorClass = "Decoder_DataTypes_" . ucfirst( strtolower( $this->dataType ) );
-			$convertor = new ConvertorClass();
+			$convertorClass = "CentralApps\CAN\Decoder_DataTypes_" . ucfirst( strtolower( $this->dataType ) );
+			$convertor = new $convertorClass();
 			$data = $convertor->convertType( $data );
+		}
+		else
+		{
+			$data = bindec( $data );
 		}
 		return $data;
 	}
