620822f61a701b919d2b404ea7a5883b4a615453
Michael Peacock <mkpeacock@gmail.com>
2012-01-27 12:49:41 +0000

    index.php cleanedup for release

--- src/index.php
+++ src/index.php
@@ -7,45 +7,12 @@ $classLoader = new SplClassLoader('CentralApps\CAN', FRAMEWORK_PATH );
 $classLoader->register();
 
 
-$message = new CentralApps\CAN\Message( '0C0', '2C01000000180000' );
-
-$decoderKeysCollection = new CentralApps\CAN\Decoder_Keys_Collection();
-$singleByteKey = new CentralApps\CAN\Decoder_Keys_SingleByte();
-$singleByteKey->setCanID( '0C0' );
-$singleByteKey->setName( 'SOC P1' );
-$singleByteKey->setUnit( "%" );
-$singleByteKey->setMultiplier( 0 );
-$singleByteKey->setOffset( 0 );
-$singleByteKey->setByte( 0 );
-$singleByteKey->setDataLength( 8 );
-$singleByteKey->setDataType( null );
-
-$decoderKeysCollection->add( $singleByteKey );
-$decoder = new CentralApps\CAN\Decoder_Standard( $decoderKeysCollection );
-$decoder->decode( $message );
+$message = new CentralApps\CAN\Message( 'canID', '0000000000000000' );
 
 $decoderKeysCollection = new CentralApps\CAN\Decoder_Keys_Collection();
 $singleByteKey = new CentralApps\CAN\Decoder_Keys_MultiByte();
-$singleByteKey->setCanID( '0C0' );
-$singleByteKey->setName( 'SOC P1' );
-$singleByteKey->setUnit( "%" );
-$singleByteKey->setMultiplier( 0 );
-$singleByteKey->setOffset( 0 );
-$singleByteKey->setMostSignificantByte( 1 );
-$singleByteKey->setLeastSignificantByte( 0 );
-$singleByteKey->setDataLength( 16 );
-$singleByteKey->setDataType( null );
-
-$decoderKeysCollection->add( $singleByteKey );
-$decoder = new CentralApps\CAN\Decoder_Standard( $decoderKeysCollection );
-$decoder->decode( $message );
-
-$message = new CentralApps\CAN\Message( '0C0', '83E7000000180000' );
-
-$decoderKeysCollection = new CentralApps\CAN\Decoder_Keys_Collection();
-$singleByteKey = new CentralApps\CAN\Decoder_Keys_MultiByte();
-$singleByteKey->setCanID( '0C0' );
-$singleByteKey->setName( 'SOC P1' );
+$singleByteKey->setCanID( 'canID' );
+$singleByteKey->setName( 'Some Data' );
 $singleByteKey->setUnit( "%" );
 $singleByteKey->setMultiplier( 0 );
 $singleByteKey->setOffset( 0 );
@@ -62,36 +29,5 @@ foreach( $units as $unit )
 	echo $unit;
 }
 
-$message = new CentralApps\CAN\Message( '0C0', '0183E70000180000' );
-
-$decoderKeysCollection = new CentralApps\CAN\Decoder_Keys_Collection();
-$singleByteKey = new CentralApps\CAN\Decoder_Keys_MultiByte();
-$singleByteKey->setCanID( '0C0' );
-$singleByteKey->setName( 'SOC' );
-$singleByteKey->setUnit( "%" );
-$singleByteKey->setMultiplier( 0 );
-$singleByteKey->setOffset( 0 );
-$singleByteKey->setMostSignificantByte( 1 );
-$singleByteKey->setLeastSignificantByte( 2 );
-$singleByteKey->setDataLength( 16 );
-$singleByteKey->setDataType( 'short' );
-
-$decoderKeysCollection->add( $singleByteKey );
-$decoder = new CentralApps\CAN\Decoder_Multiplex( $decoderKeysCollection );
-$decoder->setMultiplexByte(0);
-$units = $decoder->decode( $message );
-foreach( $units as $unit )
-{
-	echo $unit;
-}
-
-/**
- * To create a CAN message decoder we need to create a message object, and lookup the type of message this is (multiplex or standard)
- * with the message type, we need to lookup the 
- * 
- * 
- * 
- * 
- */
 
 
