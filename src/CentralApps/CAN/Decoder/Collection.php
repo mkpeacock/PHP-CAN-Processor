acd46b89a7a14fabcf01456dc3f0dcbcee92a48f
Michael Peacock <michael@centralapps.co.uk>
2012-01-28 20:02:53 +0000

    cast the can ID as a string when adding to the collection array, stops nasty error

--- src/CentralApps/CAN/Decoder/Collection.php
+++ src/CentralApps/CAN/Decoder/Collection.php
@@ -11,12 +11,24 @@ class Decoder_Collection implements \IteratorAggregate, \Countable {
 	
 	public function add( $object )
 	{
-		$this->collection[ $object->getCanID() ] = $object;
+		$this->collection[ (String) $object->getCanID() ] = $object;
 	}
 	
 	public function count()
 	{
 		return count( $this->collection );
+	}
+	
+	public function decode( Message $message )
+	{
+		if( array_key_exists( $message->getCanID(), $this->collection ) )
+		{
+			return $this->collection[ $message->getCanID() ]->decode( $message );
+		}
+		else
+		{
+			throw new \Exception('No decoder found for that CAN ID');
+		}
 	}
 	
 	
