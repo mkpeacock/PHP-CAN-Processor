14a321810d06563c01170656934c1b5f1102d591
Michael Peacock <mkpeacock@gmail.com>
2012-01-30 10:51:17 +0000

    fixed index.php

--- src/index.php
+++ src/index.php
@@ -7,27 +7,28 @@ $classLoader = new SplClassLoader('CentralApps\CAN', FRAMEWORK_PATH );
 $classLoader->register();
 
 
-$message = new CentralApps\CAN\Message( 'canID', '0000000000000000' );
+$message = new CentralApps\CAN\Message( 'ABC', '0000000000000000' );
 
-$decoderKeysCollection = new CentralApps\CAN\Decoder_Keys_Collection();
-$singleByteKey = new CentralApps\CAN\Decoder_Keys_MultiByte();
-$singleByteKey->setCanID( 'canID' );
-$singleByteKey->setName( 'Some Data' );
-$singleByteKey->setUnit( "%" );
-$singleByteKey->setMultiplier( 0 );
-$singleByteKey->setOffset( 0 );
-$singleByteKey->setMostSignificantByte( 0 );
-$singleByteKey->setLeastSignificantByte( 1 );
-$singleByteKey->setDataLength( 16 );
-$singleByteKey->setDataType( 'short' );
 
-$decoderKeysCollection->add( $singleByteKey );
-$decoder = new CentralApps\CAN\Decoder_Standard( $decoderKeysCollection );
+$multiByteKey = new CentralApps\CAN\Decoder_Keys_MultiByte();
+$multiByteKey->setCanID( 'ABC' );
+$multiByteKey->setName( 'Piston_position' );
+$multiByteKey->setUnit( "mm" );
+$multiByteKey->setMultiplier( 0.01 );
+$multiByteKey->setOffset( 0 );
+$multiByteKey->setMostSignificantByte( 2 );
+$multiByteKey->setLeastSignificantByte( 3 );
+$multiByteKey->setDataLength( 16 );
+$multiByteKey->setDataType( 'short' );
+
+$decoder = new CentralApps\CAN\Decoder_Multiplex();
+$decoder->setMultiplexByte(0);
+$decoder->setNameSuffix('_Pison%1$d');
+$decoder->add( $multiByteKey );
 $units = $decoder->decode( $message );
 foreach( $units as $unit )
 {
-	echo $unit;
+    echo $unit;
 }
-
 
 
